25b29bfd2e44ef85d9572a96dfb4ca937eec5e9e
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-01-18 00:55:18 -0600

    Fix linters issues

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+    "ignorePatterns": [
+        "dist/",
+        "build/",
+        "webpack.config.js"
+    ]
+}
